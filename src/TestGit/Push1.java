eb46b32667eb792c9a1f4a52dfb360c1ceead42e
DariaPinchuk <darianaprovode@gmail.com>
2020-06-16 22:39:40 +0300

    New changes

--- src/TestGit/Push1.java
+++ src/TestGit/Push1.java
@@ -4,7 +4,9 @@ public class Push1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+System.out.println(" Syntax Technologies");
 
+System.out.println("Changes");
 	}
 
 }
